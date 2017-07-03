47dd26e261f1d7d054cd8795820c661ed9598488
Hack Reactor Student <student@hackreactor.com>
2017-07-03 12:31:08 -0700

    stringifyJSON done

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,40 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  //inpuut some object or item
+  //output that object 'stringifyed' if possible
+  if (typeof obj === 'number' || typeof obj === 'boolean'){
+    return obj.toString();
+  }
+  if (typeof obj === 'string') {
+    return '"' + obj + '"';
+  }
+
+  if (Array.isArray(obj)){
+    var result = '[';
+
+    for (var i = 0; i < obj.length; i++){
+      result += stringifyJSON(obj[i]) + ",";
+    }
+
+    if (obj.length > 0){
+      result  = result.slice(0,-1);
+    }
+
+    result += "]";
+
+    return result;
+  }
+  if (obj === null) {
+    return 'null';
+  }
+  if(typeof obj === 'object') {
+    var result = [];
+    for (var keys in obj) {
+      if (obj.hasOwnProperty(keys) && obj[keys] !== undefined && typeof(obj[keys]) !== 'function' ) {
+        result.push(stringifyJSON(keys) + ':' + stringifyJSON(obj[keys]));
+      }
+    }
+    return  '{' + result.join(',') + '}'
+  }
 };
