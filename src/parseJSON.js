468361c6fa326c9a3e4d2047924ff2577589c28e
ddarrach314 <ddarrach314@gmail.com>
2017-07-04 12:05:07 -0700

    cleanup

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -3,14 +3,5 @@
 
 // but you're not, so you'll write it from scratch:
 var parseJSON = function(json) {
-  // your code goes here
-  var arrPatt = /^\[(.*)]$/;
-  var objPatt = /^{(.*)}$/;
 
-  if(arrPatt.test(json)) {
-    return [];
-  }
-  if (objPatt.test(json)) {
-    return {};
-  }
 };
